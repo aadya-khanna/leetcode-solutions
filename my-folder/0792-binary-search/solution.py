d846bd2d4f0c0b19a2f8f2280952450120bd4f5d
Aadya <102467098+aadya-khanna@users.noreply.github.com>
2026-09-25 20:30:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.3 MB (73.75%)

---
+++ my-folder/0792-binary-search/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        # middle = ( right + left ) / 2
+
+        while(left <= right):
+            middle = ( right + left ) / 2
+
+            if (nums[middle] == target):
+                return middle
+            elif (nums[middle] < target):
+                left = middle + 1
+            else:
+                right = middle - 1
+
+        return -1
+        
